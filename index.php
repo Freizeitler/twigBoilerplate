9c11a25389327b26a7f78540fca7ab35da6e6493
Henry Zeitler <hzeitler@menadwork.com>
2017-10-15 20:35:53 +0200

    pass json file

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     require_once 'vendor/autoload.php';
 
     $loader = new Twig_Loader_Filesystem('patterns/pages/');
-    $twig = new Twig_Environment($loader);
 
     $json = file_get_contents('data/data.json');
     $obj = json_decode($json);
-    
-    echo $twig->render('pageIndex.twig');
+
+    $twig = new Twig_Environment($loader);
+
+    echo $twig->render('pageIndex.twig', array('data' => $obj));
 
 ?>
