f275a0afe3e61d17b50656ce130abe884e05cc7d
Henry Zeitler <hzeitler@menadwork.com>
2017-10-15 22:44:42 +0200

    try to implement gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
+'use strict';
+
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
-gulp.task('build', function() {
-  // place code for your default task here
+gulp.task('sassPatterns', function() {
+  return gulp.src('./patterns/**/*.scss')
+  .pipe(({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./patterns/**/*.min.css'));
 });
 
-gulp.task('stream', function () {
-    // Endless stream mode 
-    return watch('patterns/**/*.scss', { ignoreInitial: false })
-        .pipe(gulp.dest('build'));
+gulp.task('sassGlobals', function() {
+  return gulp.src('./scss/app.scss')
+  .pipe(({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./assets/css/*.min.css'));
 });
+
+// watchers:
+gulp.task('watchPatterns', function () {
+    return watch('./patterns/**/*.scss')
+        .pipe(gulp.dest('sassPatterns'));
+});
+
+gulp.task('watchGlobal', function () {
+  return watch('./scss/*.scss')
+      .pipe(gulp.dest('sassGlobals'));
+});
+
+// default
+gulp.task('default', [ 'watchPatterns', 'watchGlobal' ]);
--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
     require_once 'vendor/autoload.php';
 
-    $loader = new Twig_Loader_Filesystem('patterns/pages/');
+    $loader = new Twig_Loader_Filesystem('patterns/');
 
     $json = file_get_contents('data/data.json');
     $obj = json_decode($json);
 
     $twig = new Twig_Environment($loader);
 
-    echo $twig->render('pageIndex.twig', array('data' => $obj));
+    echo $twig->render('pages/pageIndex.twig', array(
+        'data' => $obj
+    ));
 
 ?>
