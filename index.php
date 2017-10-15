431dd576bd39a52e205ca141b4b28da9bedb17dc
Henry Zeitler <hzeitler@menadwork.com>
2017-10-15 02:56:56 +0200

    get data

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 
     $loader = new Twig_Loader_Filesystem('patterns/pages/');
     $twig = new Twig_Environment($loader);
+
+    $json = file_get_contents('data/data.json');
+    $obj = json_decode($json);
     
     echo $twig->render('pageIndex.twig');
 
