d45ca822d9163ad5402732268612d9df896541c7
Henry Zeitler <hzeitler@menadwork.com>
2017-10-22 01:01:12 +0200

    add uglify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,56 @@
 'use strict';
 
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-sass = require('gulp-sass');
+  watch = require('gulp-watch'),
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify'),
+  pump = require('pump'),
+  concat = require('gulp-concat');
+  
 
 // gulp-sass
-gulp.task('sassPatterns', function() {
+gulp.task('sassPatterns', function () {
   return gulp.src('patterns/**/*.scss')
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(gulp.dest(function (file) {
-    return file.base;
-  }));
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(function (file) {
+      return file.base;
+    }));
+});
+gulp.task('sassGlobals', function () {
+  return gulp.src('scss/app.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('assets/css/'));
 });
 
-gulp.task('sassGlobals', function() {
-  return gulp.src('scss/app.scss')
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(gulp.dest('assets/css/'));
+// gulp-uglify
+gulp.task('compress', function (cb) {
+  pump([
+      gulp.src('patterns/**/*.js'),
+      uglify(),
+      gulp.dest(function (file) {
+        return file.base;
+      })
+    ],
+    cb
+  );
+});
+
+// gulp-concat
+gulp.task('scripts', function() {
+  return gulp.src('patterns/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./assets/js/'));
 });
 
 // gulp-watch
 gulp.task('watch', function () {
-    gulp.watch('patterns/**/*.scss', ['sassPatterns']);    
-    gulp.watch('scss/*.scss', ['sassGlobals']);
+  gulp.watch('patterns/**/*.scss', ['sassPatterns']);
+  gulp.watch('scss/*.scss', ['sassGlobals']);
 });
 
 // default
-gulp.task('default', [ 'watch' ]);
+gulp.task('default', ['watch']);
